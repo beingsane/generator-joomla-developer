17b40c3633fd497ca8b1145c13517f5b64b6278b
Brian Bolli <brian.bolli@arctg.com>
2015-03-18 21:54:54 -0500

    started build out of packager feature

--- package/index.js
+++ package/index.js
@@ -3,6 +3,7 @@ var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
 var path = require('path');
+var packager = require('./packager');
 
 module.exports = yeoman.generators.Base.extend({
 
@@ -14,94 +15,89 @@ module.exports = yeoman.generators.Base.extend({
 		var done = this.async();
 		// Have Yeoman greet the user.
 		this.log(yosay(
-			'Welcome to the ' + chalk.red('JoomlaDeveloper') + ' module generator!'
+			'Welcome to the ' + chalk.red('JoomlaDeveloper') + ' packager!'
 		));
 
-		var prompts =
+		var determineType =
 		[
 			{
-				type : 'input',
-				name : 'camelcase',
-				message : 'What is name of the new module using CamelCase formatting?',
-				store : true
+				type: 'list',
+				name: 'type',
+				message: 'What type of Joomla extension would you like to package?',
+				choices: ['Component', 'Module', 'Plugin', 'Template', 'Library', 'Language']
 			}
 		];
 
-		this.prompt(prompts, function (props) {
-			this.camelcase = props.camelcase;
-			done();
+		this.prompt(determineType, function(props)
+		{
+			var name = props.type.toLowerCase();
+			name = name + 's';
+
+			var extensions = this.config.get(name);
+			this.type = name;
+
+			var options = [];
+
+			for (var i = 0; i < extensions.length; i++)
+			{
+				options.push(extensions[i].formal.name);
+			}
+
+			if (options.length === 0)
+			{
+				done('There are no extensions of type ' + props.type + ' to choose from!');
+			}
+
+			var determineExtension =
+			[
+			 	{
+			 		type: 'list',
+			 		name: 'extension',
+			 		message: 'Which ' + props.type + ' would you like to package?',
+			 		choices: options
+			 	}
+			];
+
+			this.prompt(determineExtension, function(props){
+				this.extension = props.extension;
+				done();
+			}.bind(this));
+
 		}.bind(this));
 	},
 
 	writing: {
-		module: function () {
-			var params = {
-					module: this.camelcase.toLowerCase(),
-					author: 'Binary Pursuits',
-					created: 'January 2015',
-					copyright: '&copy; 2011 - 2015 Binary Pursuits.  All Rights Reserved.',
-					license: 'GNU General Public License version 2 or later; see LICENSE.txt',
-					email: 'joomla@binarypursuits.com',
-					website: 'www.binarypursuits.com',
-					version: '0.0.0',
-					description: 'Test Yeoman Generator for Joomla Modules.',
-					uppercase: this.camelcase.toUpperCase(),
-					camelcase: this.camelcase,
-					languagefile: true,
-					languagecode: 'en-GB',
-					mediafolder: false
-				};
+		"package": function () {
 
-			this.fs.copyTpl(
-				this.templatePath('_manifest.xml'),
-				this.destinationPath('joomla/modules/mod_' + params.module + '/mod_' + params.module + '.xml'),
-				params
-			);
-
-			this.fs.copyTpl(
-					this.templatePath('_module.php'),
-					this.destinationPath('joomla/modules/mod_' + params.module + '/mod_' + params.module + '.php'),
-					params
-				);
-
-			this.fs.copyTpl(
-				this.templatePath('_default.php'),
-				this.destinationPath('joomla/modules/mod_' + params.module + '/tmpl/default.php'),
-				params
-			);
-
-			this.fs.copyTpl(
-				this.templatePath('_helper.php'),
-				this.destinationPath('joomla/modules/mod_' + params.module + '/helper.php'),
-				params
-			);
-
-			if (params.languagefile === true && typeof params.languagecode !== "undefined")
+			function loadDetails(extensions, name)
 			{
 
-				this.fs.copyTpl(
-					this.templatePath('_language.ini'),
-					this.destinationPath('joomla/language/' + params.languagecode + '/' + params.languagecode + '.mod_' + params.module + '.ini'),
-					params
-				);
 
-				this.fs.copyTpl(
-					this.templatePath('_language.sys.ini'),
-					this.destinationPath('joomla/language/' + params.languagecode + '/' + params.languagecode + '.mod_' + params.module + '.sys.ini'),
-					params
-				);
+				for (var i = 0; i < extensions.length; i++)
+				{
+					if (extensions[i].formal.name == name)
+					{
+						return extensions[i];
+					}
+				}
+
+				return false;
 
 			}
 
-			this.fs.copyTpl(
-				this.templatePath('index.html'),
-				this.destinationPath('joomla/modules/mod_' + params.module + '/index.html')
-			);
+			var ioFileOperations = function(template, destination)
+			{
+				this.fs.copyTpl(source, destination);
+			}.bind(this);
 
-			this.fs.copyTpl(
-				this.templatePath('index.html'),
-				this.destinationPath('joomla/modules/mod_' + params.module + '/tmpl/index.html')
-			);
+			var extensions = this.config.get(this.type);
+			var target = loadDetails(extensions, this.extension);
+
+			packager.type(this.type);
+			packager.load(target);
+
+
+
 		}
 	},
 
